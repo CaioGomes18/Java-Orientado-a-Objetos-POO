4ef91bff546a4957414c0d772b600ad907f8a994
CaioGomes18 <caiomarcelinog@gmail.com>
2024-02-29 12:15:50 -0300

    Primeiro commit POO

---
+++ Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex1/School.java
@@ -0,0 +1,30 @@
+package edu.caio.POO.Ex1;
+
+public class School {
+	
+	public static void main(String [] args) {
+		
+		
+		Student student1 = new Student();
+		
+		student1.name = "José";
+		student1.age = 18;
+		student1.color = "white";
+		student1.sex = "masculine";
+		
+		Student student2 = new Student();
+		
+		student2.name = "Julia";
+		student2.age = 19;
+		student2.color = "Black";
+		student2.sex = "Feminine";
+		
+		Student student3 = new Student();
+		
+		student3.name = "Carlos";
+		student3.age = 17;
+		student3.color = "Black";
+		student3.sex = "masculine";
+	}
+	
+}
---
+++ Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex1/Student.java
@@ -0,0 +1,20 @@
+package edu.caio.POO.Ex1;
+
+public class Student {
+	String name;
+	int age;
+	String color;
+	String sex;
+	
+	void eating(){
+		/*nosso código aqui;*/
+	}
+	
+	void running() {
+		//Nosso código aqui
+	}
+	
+	void drinking(){
+		
+	}
+}
