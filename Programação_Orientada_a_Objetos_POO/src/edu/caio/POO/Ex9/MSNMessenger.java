366201fa1b133cea42aa74c7e6ce7693ad8cb4ec
CaioGomes18 <caiomarcelinog@gmail.com>
2024-03-12 22:28:17 -0300

    estudando mais um pouco sobre encapsulamento

--- Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex9/MSNMessenger.java
+++ Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex9/MSNMessenger.java
@@ -1,5 +1,22 @@
 package edu.caio.POO.Ex9;
 
 public class MSNMessenger {
-
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem");
+		salvarHistoricoMensagem();
+	}
+	
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando historico da mensagem");
+	}
+	
 }
--- Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex9/TesteMensagem.java
+++ Programação_Orientada_a_Objetos_POO/src/edu/caio/POO/Ex9/TesteMensagem.java
@@ -1,5 +1,12 @@
 package edu.caio.POO.Ex9;
 
 public class TesteMensagem {
-
+	public static void main(String [] args) {
+		MSNMessenger sms = new MSNMessenger();
+		
+		sms.enviarMensagem();
+		
+		sms.receberMensagem();
+				
+	}
 }
